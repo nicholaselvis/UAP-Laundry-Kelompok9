16ce552c61d7d3aaafd98f9c1021c7c5fbd32ed6
pelatihan-git <jenitalusii@gmail.com>
2023-11-08 06:23:43 +0700

    view sidebar riwayat_cust

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,4 +14,4 @@ $routes->get('/', 'Home::index');
 $routes -> get('/dashboard_admin', [AdminController::class, 'index']);
 $routes -> get('/dashboard_kasir', [KasirController::class, 'index']);
 $routes -> get('/dashboard_cust', [CustController::class, 'index']);
-
+$routes -> get('/riwayat_cust', [CustController::class, 'riwayat']);
