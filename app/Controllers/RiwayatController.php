a014114885d0ac7dab744bd6794ba4ef6ab9863b
AzzahraAdellia <azzahraadellia52@gmail.com>
2023-11-21 15:29:21 +0700

    up

--- app/Controllers/RiwayatController.php
+++ app/Controllers/RiwayatController.php
@@ -3,11 +3,14 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\TransaksiModel;
 
 class RiwayatController extends BaseController
 {
     public function index()
-    {
-        return view("riwayat_transaksi");
+    {   
+        $transaksi = new TransaksiModel();
+        $data = $transaksi->getTransaksi();
+        return view("riwayat_transaksi",$data);
     }
 }
