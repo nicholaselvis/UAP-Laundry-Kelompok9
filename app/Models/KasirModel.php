c0d3b3e8543f9a21ce646900806cdd6bcc31e2ee
nchlslvs <92220344+nicholaselvis@users.noreply.github.com>
2023-12-11 23:04:09 +0700

    -

--- app/Models/KasirModel.php
+++ app/Models/KasirModel.php
@@ -12,7 +12,7 @@ class KasirModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['id', 'nama', 'umur', 'jeniskelamin'];
 
     // Dates
     protected $useTimestamps = false;
@@ -57,9 +57,4 @@ class KasirModel extends Model
     public function deleteKasir($id){
         return $this->delete($id);
     }
-    public function getAllMesin(){
-            return $this->findAll();
-    }
-    
 }
-    
