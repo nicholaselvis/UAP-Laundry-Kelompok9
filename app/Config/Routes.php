e3ced22bff8b355ab2819e29de992cbff2fc6b6d
nchlslvs <92220344+nicholaselvis@users.noreply.github.com>
2023-12-11 19:57:01 +0700

    -

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -15,38 +15,6 @@ use App\Controllers\RiwayatController;
  */
 $routes->get('/', 'Home::index');
 
-$routes -> get('/dashboard_admin', [AdminController::class, 'index']);
-$routes -> get('/stok', [AdminController::class, 'stok']);
-$routes -> get('/kasir', [AdminController::class, 'kasir']);
-
-
-$routes -> get('/admin', [AdminController::class, 'index']);
-$routes -> get('/admin/stok', [AdminController::class, 'stok']);
-$routes -> get('/admin/kasir', [AdminController::class, 'kasir']);
-$routes -> get('/admin/create_kasir', [AdminController::class, 'create_kasir']);
-$routes -> post('/admin/kasirstore', [AdminController::class, 'kasirstore']);
-
-$routes -> get('/admin/(:any)/edit_stok', [AdminController::class, 'edit_stok']);
-$routes -> put('/admin/(:any)/update_stok', [AdminController::class, 'updateStok']);
-$routes -> delete('/admin/(:any)/apus_stok', [AdminController::class, 'destroyStok']);
-
-$routes -> get('/admin/(:any)/edit_kasir', [AdminController::class, 'edit_kasir']);
-$routes -> put('/admin/(:any)', [AdminController::class, 'updateKasir']);
-$routes -> delete('/admin/(:any)', [AdminController::class, 'destroyKasir']);
-
-$routes -> get('/admin/create_stok', [AdminController::class, 'create_stok']);
-$routes -> post('/admin/store', [AdminController::class, 'store']);
-
-
-$routes -> get('/dashboard_kasir', [KasirController::class, 'index']);
-$routes -> get('/absensi_kasir', [AbsensiController::class, 'show']);
-$routes -> get('/riwayat_transaksi', [RiwayatController::class, 'index']);
-
-$routes -> post('/absensi/store', [AbsensiController::class, 'store']);
-// $routes->get('/dashboard_kasir', [AbsensiController::class, 'dashboard_kasir']);
-
-
-
 $routes -> get('/dashboard_cust', [CustController::class, 'index']);
 
 $routes -> get('/riwayat_cust', [CustController::class, 'riwayat']);
