bf1ab51cad0e89f95e1d8eba7357723252f30b00
nchlslvs <92220344+nicholaselvis@users.noreply.github.com>
2023-12-14 02:22:18 +0700

    ---

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -26,15 +26,11 @@ $routes -> get('/admin/kasir', [AdminController::class, 'kasir']);
 $routes -> get('/admin/create_kasir', [AdminController::class, 'create_kasir']);
 $routes -> post('/admin/kasirstore', [AdminController::class, 'kasirstore']);
 
-$routes -> get('/admin/(:any)/edit_stok', [AdminController::class, 'edit_stok']);
-$routes -> put('/admin/(:any)/update_stok', [AdminController::class, 'updateStok']);
-$routes -> delete('/admin/(:any)/apus_stok', [AdminController::class, 'destroyStok']);
 
 $routes -> get('/admin/(:any)/edit_kasir', [AdminController::class, 'edit_kasir']);
 $routes -> put('/admin/(:any)', [AdminController::class, 'updateKasir']);
 $routes -> delete('/admin/(:any)', [AdminController::class, 'destroyKasir']);
 
-$routes -> get('/admin/create_stok', [AdminController::class, 'create_stok']);
 $routes -> post('/admin/store', [AdminController::class, 'store']);
 
 
